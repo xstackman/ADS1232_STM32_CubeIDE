3771d27ccc603d24ace3338e3a1c9a0fee0745c9
Carlos Lecaro <46472584+hydracrux@users.noreply.github.com>
2021-04-05 15:55:55 -0500

    Start STM32 Cube IDE Project
    - Start project for STM32F767zi Nucleo 144 board
    
    - Configure USART3 as debug port connected to STLINK v2 debuger
    
    - Configure PG2 and PG3 as GPIO to communicate with ADS1232. This will change on the future.
    
    - ADD Empty header and source files for the library.

---
+++ Core/Inc/ADS1232.h
@@ -0,0 +1,15 @@
+/*
+ * ADS1232.h
+ *
+ *  Created on: Apr 5, 2021
+ *      Author: hydra
+ */
+
+#ifndef INC_ADS1232_H_
+#define INC_ADS1232_H_
+
+
+
+
+
+#endif /* INC_ADS1232_H_ */
---
+++ Core/Src/ADS1232.cpp
@@ -0,0 +1,10 @@
+/*
+ * ADS1232.cpp
+ *
+ *  Created on: Apr 5, 2021
+ *      Author: hydra
+ */
+
+
+
+
